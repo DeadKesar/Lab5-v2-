0de711c907b9082e6cca5fd0f5692c86880cc562
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2022-04-20 09:49:36 +0300

    финал (доработка 1)

--- Lab5(v2)/Lab5(v2).cpp
+++ Lab5(v2)/Lab5(v2).cpp
@@ -12,10 +12,11 @@ void FillArray(Vector* vec, const int SIZE, const int RANGE, std::mt19937 engine
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	std::cout << "Лабораторная работа №5. \nПо теме: \"Разработка ПО в стиле ООП. Работа с файлами и потоками ввода/вывода.\"\n" <<
+		"Вариант №13 \nВыполнил: Порядин В.С." << std::endl;
 	std::mt19937 engine;
 	engine.seed(time(nullptr));
 
-	int const START = 0; // начальный индекс сортировки
 	int const SIZE = 100000000;//количество сортируемых чисел
 	int const RANGE = 1000000; // диапазон значений
 	std::vector<int> vecSTD;
@@ -30,22 +31,35 @@ int main()
 	clock_t timer2 = clock();
 	FillArray(myVector, SIZE, RANGE, engine);
 	clock_t curent2 = clock();
-
-	std::cout << static_cast<double>(curent - timer) / CLOCKS_PER_SEC << " - время генерации стандартного вектора" << std::endl;
-	std::cout << static_cast<double>(curent2 - timer2) / CLOCKS_PER_SEC << " - время генерации самодельного вектора" << std::endl;
+	double stdVectorTime = static_cast<double>(curent - timer) / CLOCKS_PER_SEC;
+	double myVectorTime = static_cast<double>(curent2 - timer2) / CLOCKS_PER_SEC;
+	std::cout << stdVectorTime << " - время генерации стандартного вектора" << std::endl;
+	std::cout << myVectorTime << " - время генерации самодельного вектора" << std::endl;
 	vecSTD.clear();
+
+	if (stdVectorTime > myVectorTime)
+	{
+		std::cout << "при заполнении структуры Вектора, моя ревлизация по какой то причине оказалась быстрее на " <<stdVectorTime - myVectorTime<< " c." << std::endl;
+	}
+	else
+	{
+		std::cout << "при заполнении структуры Вектора, стандартная реализация оказалась быстрее на " << myVectorTime - stdVectorTime << " c." << std::endl;
+	}
+
 
 	timer = clock();
 	myVector->MergeSort(0, myVector->Length());
 	curent = clock();
-	std::cout << static_cast<double>(curent - timer) / CLOCKS_PER_SEC << " - время cортировки слиянием" << std::endl;
+	double mergeTime = static_cast<double>(curent - timer) / CLOCKS_PER_SEC;
+	std::cout << mergeTime << " - время cортировки слиянием" << std::endl;
 	myVector->Clear();
 
 	FillArray(myVector, SIZE, RANGE, engine);
 	timer = clock();
 	myVector->AlternativeMergeSort(0, myVector->Length());
 	curent = clock();
-	std::cout << static_cast<double>(curent - timer) / CLOCKS_PER_SEC << " - время альтернативной сортировки слиянием (без слияния)" << std::endl;
+	double altMergeTime = static_cast<double>(curent - timer) / CLOCKS_PER_SEC;
+	std::cout << altMergeTime << " - время альтернативной сортировки слиянием (без слияния)" << std::endl;
 	myVector->Clear();
 
 
@@ -53,7 +67,8 @@ int main()
 	timer = clock();
 	myVector->RadixSort(RANGE);
 	curent = clock();
-	std::cout << static_cast<double>(curent - timer) / CLOCKS_PER_SEC << " - время поразрядной сортировки" << std::endl;
+	double radixTime = static_cast<double>(curent - timer) / CLOCKS_PER_SEC;
+	std::cout << radixTime << " - время поразрядной сортировки" << std::endl;
 	myVector->Clear();
 
 	int range = RANGE;
@@ -63,16 +78,49 @@ int main()
 	timer = clock();
 	myVector->BlockSort(range);
 	curent = clock();
-	std::cout << static_cast<double>(curent - timer) / CLOCKS_PER_SEC << " - время блочной сортировки" << std::endl;
+	double blockTime = static_cast<double>(curent - timer) / CLOCKS_PER_SEC;
+	std::cout << blockTime << " - время блочной сортировки" << std::endl;
 	myVector->Clear();
 
 
 	FillArray(myVector, SIZE, RANGE, engine);
 	timer = clock();
-	myVector->RadixSort(RANGE);
+	myVector->BlockSortUseSTD(RANGE);
 	curent = clock();
-	std::cout << static_cast<double>(curent - timer) / CLOCKS_PER_SEC << " - время блочной сортировки на STD::vector" << std::endl;
+	double stdBlockTime = static_cast<double>(curent - timer) / CLOCKS_PER_SEC;
+	std::cout << stdBlockTime << " - время блочной сортировки на STD::vector" << std::endl;
+	myVector->BobleSortTest(myVector->Length());
 	myVector->Clear();
+
+	int faster = 0;
+	double sortsTime[] = { mergeTime ,altMergeTime,radixTime,blockTime,stdBlockTime };
+	for (int i = 1; i < 5; i++)
+	{
+		if (sortsTime[i] < sortsTime[i - 1])
+			faster = i;
+	}
+	switch (faster)
+	{
+	case (0):
+		std::cout <<  "самая быстрая сортировка при заданной конфигурации - слиянием" << std::endl;
+		break;
+	case (1):
+		std::cout <<  "самая быстрая сортировка при заданной конфигурации - слиянием(Альтернативная)" << std::endl;
+		break;
+	case (2):
+		std::cout <<  "самая быстрая сортировка при заданной конфигурации - поразрядная" << std::endl;
+		break;
+	case (3):
+		std::cout <<  "самая быстрая сортировка при заданной конфигурации - блочная сортировка" << std::endl;
+		break;
+	case (4):
+		std::cout <<  "самая быстрая сортировка при заданной конфигурации - блочная сортировка на STD::vector" << std::endl;
+		break;
+	default:
+		break;
+	}
+
+
 
 
 	//FillArray(myVector, SIZE, RANGE, engine);
